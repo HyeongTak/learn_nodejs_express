2c5a08ab1377448438279333ebe4966e830fda98
전형탁 <andy395023@gmail.com>
2017-11-30 17:01:03 +0900

    Add files via upload

--- router/main.js
+++ router/main.js
@@ -1,8 +1,8 @@
-module.exports = function(app){
+module.exports = function(app, fs){
     app.get('/', function(req, res){
-        res.render('index.html');
-    });
-    app.get('/about',function(req, res){
-        res.render('about.html');
+        res.render('index', {
+            title: "MY HOMEPAGE", // JSON 데이터를 render메소드의 두번째 인자로 전달
+            length: 5 // 페이지에서 데이터를 사용 가능
+        });
     });
 }
