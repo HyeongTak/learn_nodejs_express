c6d71fd0b191badaf6e57d01eed5e886ce011fc2
전형탁 <andy395023@gmail.com>
2017-12-01 13:57:34 +0900

    Add files via upload

--- router/main.js
+++ router/main.js
@@ -1,8 +1,12 @@
 module.exports = function(app, fs){
     app.get('/', function(req, res){
+        var sess = req.session;
+
         res.render('index', {
             title: "MY HOMEPAGE", // JSON 데이터를 render메소드의 두번째 인자로 전달
-            length: 5 // 페이지에서 데이터를 사용 가능
+            length: 5, // 페이지에서 데이터를 사용 가능
+            name: sess.name,
+            username: sess.username
         });
     });
 
@@ -20,7 +24,7 @@ module.exports = function(app, fs){
         });
     });
 
-
+    // 데이터 저장
     app.post('/addUser/:username', function(req, res){
         var result = {  };
         var username = req.params.username;
@@ -56,6 +60,7 @@ module.exports = function(app, fs){
         });
     });
 
+    // 데이터 삭제
     app.delete('/deleteUser/:username', function(req, res){
         var result = { };
         //LOAD DATA
@@ -76,10 +81,55 @@ module.exports = function(app, fs){
                 result["success"] = 1;
                 res.json(result);
                 return;
-            })
-        })
-
-    })
-
+            });
+        });
+    });
     
+    // 로그인
+    app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+
+            if(!users[username]){
+                // Username을 찾을 수 없을 떄
+                result["seccess"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password){
+                result["seccess"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            }else{
+                result["seccess"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        });
+    });
+
+    //로그아웃
+    app.get('/logout', function(req, res){
+        sess = req.session;
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            });
+        }else{
+            res.redirect('/');
+        }
+    });
 }
